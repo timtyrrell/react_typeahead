e462debf784597536dd1210e2a4427b390d6ab57
Tim Tyrrell <tim@tyrrell.io>
2014-12-06 15:31:21 -0600

    Switch forEach to map

--- src/typeAheadList.js
+++ src/typeAheadList.js
@@ -1,11 +1,10 @@
 var TypeAheadList = React.createClass({
   render: function() {
-    var postListItems = []
-    if(this.props.posts){
-      this.props.posts.forEach(function(post){
-        postListItems.push(<li key={post.ID}>{post.title}</li>)
+    var postListItems = this.props.posts.map(function(post){
+        return(
+          <li key={post.ID}>{post.title}</li>
+        )
       })
-    }
 
     return (
       <ol>
